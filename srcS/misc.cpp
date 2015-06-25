096397d68e9ffae870cac9c535e371abf292eefe
Marco Zerbinati <zerbinati.marco@gmail.com>
2015-06-25 18:27:16 +0200

    Infostring
    Infostring version

--- srcS/misc.cpp
+++ srcS/misc.cpp
@@ -31,7 +31,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-static const string Version = "v5.4";
+const string Version = "";
 
 /// Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 /// cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -107,10 +107,10 @@ const string engine_info(bool to_uci) {
   if (Version.empty())
   {
       date >> month >> day >> year;
-      ss << setw(2) << day << setw(2) << (1 + months.find(month) / 4) << year.substr(2) << "b";
+      ss << setw(2) << day << setw(2) << (1 + months.find(month) / 4) << year.substr(2) << "";
   }
 
-  ss << (Is64Bit ? " 64" : " 32")
+  ss << (Is64Bit ? " 64-bit" : " 32-bit")
      << (HasPext ? " BMI2" : ( UseAVX ? " AVX" : (HasPopCnt ? " POPCNT" : "")))
      << (to_uci  ? "\nid author ": " by ")
      << "Marco Zerbinati";
